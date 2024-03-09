8f47cfb0179a403c6dbe098e02f2b4353fb141ab
amirkhusaini <amirkhusaini2405@gmail.com>
2024-03-09 19:21:19 +0800

    update whatsapp on 09032024

--- resources/views/home/footer.blade.php
+++ resources/views/home/footer.blade.php
@@ -4,7 +4,7 @@
    <div class="footer_col">
       <h4>Our Sosials Media</h4>
       <a href="https://www.facebook.com/phototherapyjohor?mibextid=ZbWKwL"><i class="ri-facebook-circle-fill"></i> Facebook</a>
-      <a href="#"><i class="ri-whatsapp-fill"></i> Whatsapp</a>
+      <a href="https://wa.me/127843100?text=Hi, Saya hendak membuat tempahan lampu Jaundice." target="_blank"><i class="ri-whatsapp-fill"></i> Whatsapp</a>
    </div>
    <div class="footer_bar">
       Copyright © 2024 Web Design Mastery. All rights reserved.
